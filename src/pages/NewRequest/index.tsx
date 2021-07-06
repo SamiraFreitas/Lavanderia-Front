687768145de61455cc15a2382f59f2490013e1d4
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2021-07-06 12:11:15 -0300

    atualizacao-newRequest

--- src/pages/NewRequest/index.tsx
+++ src/pages/NewRequest/index.tsx
@@ -10,7 +10,47 @@ export function NewRequest() {
       
       <main>
         <div className="board">
+        <form >
+            <div >
+              <b> CPF</b>
+              <input 
+                type="text" 
+                placeholder="Digite o seu cpf"
+                
+              />
+              <b>Kg</b>
+              <input 
+                type="text" 
+                placeholder="Qtd roupa kg"
+              />
+            </div>
+            
+            <div>
+              <b>Tipo de Lavagem</b>
+              <select>
+                        <option value="seco">A seco</option>
+                        <option value="Agua">Agua</option>
+              </select>
+            </div>
+          </form>
+          <div className="section">
+            <h3>Previsão</h3>
+            <div className="results" >
+              <div>
+                <label>Custo médio mensal</label>
+               
+              </div>
+              
+              <div>
+                <label>Peso mínimo a ser lavado</label>
+                
+              </div>
 
+              <div>
+                <label>Lucro</label>
+              </div>
+            </div>
+          </div>
         </div>
         <Button onClick={() => console.log('Salvando...')}>Cadastrar Pedido</Button>
       </main>
