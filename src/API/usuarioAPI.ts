973d6f81c5d9f7119db50bc88af048b98e08d083
Francisco Vasconcelos <fr4nk2000@gmail.com>
2021-08-07 14:14:55 -0300

    Criadas funções para API de Usuario

--- src/API/usuarioAPI.ts
+++ src/API/usuarioAPI.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-import baseURL from './URLconfig'
-
+import baseURL from "./URLconfig";
 
 interface Usuario {
 	nome: string;
@@ -13,7 +12,7 @@ interface Usuario {
 class UsuarioAPI {
 	getAllUsuarios = async () => {
 		const response = await axios
-			.get(baseURL + '/usuario')
+			.get(baseURL + "/usuario")
 			.then((res) => {
 				console.log(res.data);
 				return res.data as Usuario[];
@@ -23,8 +22,96 @@ class UsuarioAPI {
 		return response;
 	};
 
-}
+	getUsuarioByCPF = async (cpf: string) => {
+		const response = await axios
+			.get(baseURL + `/getusuario/${cpf}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Usuario;
+			})
+			.catch((err) => {
+				console.log(err)
+				return null
+			});
 
-export {}
+		return response;
+	};
+
+	getAllUsuariosByCNPJ_LAVANDERIA = async (cnpj: string) => {
+		const response = await axios
+			.get(baseURL + `/${cnpj}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Usuario[];
+			})
+			.catch((err) => {
+				console.log(err)
+				return null
+			});
+
+		return response;
+	};
+
+	createUsuario = async (body: Usuario) => {
+		const response = await axios
+			.post(baseURL + "/usuario/create", body)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as string;
+			})
+			.catch((err) => {
+				console.log(err)
+				return null
+			});
+
+		return response;
+	};
+
+	deleteUsuario = async (cpf: string) => {
+		const response = await axios
+			.delete(baseURL + `/usuario/d=${cpf}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as string;
+			})
+			.catch((err) => {
+				console.log(err)
+				return null
+			});
+
+		return response;
+	};
+
+	updateUsuario = async (body: Usuario) => {
+		const response = await axios
+			.put(baseURL + `/usuario/up=${body.cpf}`, body)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Usuario
+			})
+			.catch((err) => {
+				console.log(err)
+				return null
+			})
+		
+		return response;
+	}
+
+	doLogin = async (cpf: string, password: string) => {
+		
+		const user = await this.getUsuarioByCPF(cpf)
+
+		if (user !== null) {
+			if (user.senha === password) {
+				console.log('Login success')
+				return [user, true]
+			} else {
+				return [null, false]
+			}
+		} else {
+			return [null, false]
+		}
+	}
+}
 
 export default UsuarioAPI;
