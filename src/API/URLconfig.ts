3c45ebdced0c72fe7ec78e72fe8bf18f134d0a62
Francisco Vasconcelos <fr4nk2000@gmail.com>
2021-08-11 16:25:44 -0300

    Criação de da API de custos e de pedido

--- src/API/URLconfig.ts
+++ src/API/URLconfig.ts
@@ -1,3 +1,3 @@
 const baseURL = "http://localhost:1337";
 
-export default baseURL
+export default baseURL;
---
+++ src/API/pedidoAPI.ts
@@ -0,0 +1,128 @@
+import axios from "axios";
+import baseURL from "./URLconfig";
+
+interface Pedido {
+	id: number;
+	peso: number;
+	tipo: number;
+	status: string;
+	preco: number;
+	cpf_cliente: string;
+}
+
+interface PedidoDTO {
+	peso: number;
+	tipo: number;
+	status: string;
+	preco: number;
+	cpf_cliente: string;
+}
+
+class PedidoAPI {
+	getAllPedidosByLavanderia = async (cnpj: string) => {
+		const response = await axios
+			.get(baseURL + `/lavanderia/pedido/${cnpj}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Pedido[];
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+			});
+
+		return response;
+	};
+
+	getAllPedidosByCliente = async (cpf: string) => {
+		const response = await axios
+			.get(baseURL + `/cliente/pedido/${cpf}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Pedido[];
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+			});
+
+		return response;
+	};
+
+	getAllPedidosByLavanderiaAndNotEntregue = async (cnpj: string) => {
+		const response = await axios
+			.get(baseURL + `/lavanderia/pedido/pendente/${cnpj}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Pedido[];
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+			});
+
+		return response;
+	};
+
+	getPedido = async (id: number) => {
+		const response = await axios
+			.get(baseURL + `/pedido/id=${id}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as Pedido;
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+			});
+
+		return response;
+	};
+
+	updatePedido = async (body: PedidoDTO, id: number) => {
+		const response = await axios
+			.put(baseURL + `/pedido/up=${id}`, body)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as string;
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+			});
+
+		return response;
+	};
+
+	deletePedido = async (id: number) => {
+		const response = await axios
+			.delete(baseURL + `/pedido/del=${id}`)
+			.then((res) => {
+				console.log(res.data);
+				return res.data as string;
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+			});
+
+		return response;
+	};
+
+	createPedido = async (body: PedidoDTO) => {
+		const response = await axios
+			.post(baseURL + "/pedido")
+			.then((res) => {
+				console.log(res.data);
+				return res.data as string;
+			})
+			.catch((err) => {
+				console.log(err);
+				return null;
+      });
+    
+    return response;
+	};
+}
+
+export default PedidoAPI
